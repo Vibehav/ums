1312d0597219d6394b6cdba8a253ec1283471d35
vibehav <vibehav@protonmail.com>
2026-06-22 11:45:58 +0530

    fix:hidden annotation to make swagger work

--- src/main/java/com/example/ums/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ums/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.example.ums.exception;
 
-import jakarta.persistence.OptimisticLockException;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 import java.util.List;
-
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
